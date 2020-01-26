994c0113f30d26f1f8dfe16445785af112d3da8d
swanner@gmx.ch <swanner@gmx.ch>
2020-01-26 15:10:54 -0500

    increased size

--- SimpleGraphic/Main.java
+++ SimpleGraphic/Main.java
@@ -4,7 +4,8 @@
 // First graphic, using an example from the internet.
 //
 // History:
-// 01/11/2020 Initial version
+// 01/11/2020 Initial version (StW)
+// 01/26/2020 Changed size of oval (StW)
 //*******************************************************************
 
 import java.awt.Canvas;
@@ -15,13 +16,13 @@ public class Main extends Canvas {
   public static void main(String[] args) {
     JFrame frame = new JFrame("My Drawing");
     Canvas canvas = new Main();
-    canvas.setSize(400, 400);
+    canvas.setSize(800, 800);
     frame.add(canvas);
     frame.pack();
     frame.setVisible(true);
   }
 
   public void paint(Graphics g) {
-    g.fillOval(100, 100, 200, 200);
+    g.fillOval(200, 200, 400, 400);
   }
 }
