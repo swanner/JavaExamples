7227917d03c0707596b11747a92d364153534e31
swanner@gmx.ch <swanner@gmx.ch>
2020-02-18 21:23:49 -0500

    close scanner at the end

--- GuessMyNumber/Main.java
+++ GuessMyNumber/Main.java
@@ -12,42 +12,44 @@ public class Main {
         int randomNumber, lowerLimit, upperLimit, guessNumber=0, tries=0;
         Scanner in = new Scanner(System.in);
 
-	// get information about the random number range 
+		// get information about the random number range 
         System.out.print("Lower limit of the secret number (press enter for 100): ");
         try {
-		lowerLimit = Integer.parseInt(in.nextLine());
-	} catch (Exception e) {
-		lowerLimit = 0;
-	}
+			lowerLimit = Integer.parseInt(in.nextLine());
+		} catch (Exception e) {
+			lowerLimit = 0;
+		}
         System.out.print("Upper limit of the secret number (press enter for 0): ");
-	try {
+		try {
         	upperLimit = Integer.parseInt(in.nextLine());
-	} catch (Exception e) {
-		upperLimit = 100;
-	}
-	// generate the random number with the given boundaries
+		} catch (Exception e) {
+			upperLimit = 100;
+		}
+
+		// generate the random number with the given boundaries
         randomNumber = (int) (Math.random()*((upperLimit-lowerLimit)+1))+lowerLimit;
 
         System.out.println("Try to guess the secret number. ");
-	System.out.println("Hint: The number is somewhere between " + lowerLimit + " and " +  upperLimit + ".");
-	do {
-		try {
-			guessNumber = Integer.parseInt(in.nextLine());
-			tries++;
-			if (guessNumber==randomNumber){
-        			System.out.println("Awesome! You're right. It took you " + tries + " tries to find the secret number " + randomNumber + ".");
-				break;        	}
-        		else if (guessNumber > randomNumber){
-        			System.out.println("Nice try, but your guess is too high.");
-          			System.out.print("Try again: ");
-        		}
-        		else if (guessNumber < randomNumber){
-          			System.out.println("Nice try, but your guess is too low.");
-          			System.out.print("Try again: ");
-        		}
-		} catch (Exception e) {
-			System.out.print("Enter a real number: ");
-		}
-	} while (true);
-    }
+		System.out.println("Hint: The number is somewhere between " + lowerLimit + " and " +  upperLimit + ".");
+		do {
+			try {
+				guessNumber = Integer.parseInt(in.nextLine());
+				tries++;
+				if (guessNumber==randomNumber){
+						System.out.println("Awesome! You're right. It took you " + tries + " tries to find the secret number " + randomNumber + ".");
+					break;        	}
+					else if (guessNumber > randomNumber){
+						System.out.println("Nice try, but your guess is too high.");
+						System.out.print("Try again: ");
+					}
+					else if (guessNumber < randomNumber){
+						System.out.println("Nice try, but your guess is too low.");
+						System.out.print("Try again: ");
+					}
+			} catch (Exception e) {
+				System.out.print("Enter a real number: ");
+			}
+		} while (true);
+		in.close();	
+	}
 }
